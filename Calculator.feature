5af9de833c2ef2127dfb39a208a5c69f877a366e
Nagasunder <76392496+Nagasunder@users.noreply.github.com>
2021-06-08 22:31:14 +0100

    Add files via upload

---
+++ Calculator.feature
@@ -0,0 +1,19 @@
+﻿Feature: Calculator
+![Calculator](https://specflow.org/wp-content/uploads/2020/09/calculator.png)
+Simple calculator for adding **two** numbers
+
+Link to a feature: [Calculator](SpecFlowDateCalculator/Features/Calculator.feature)
+***Further read***: **[Learn more about how to generate Living Documentation](https://docs.specflow.org/projects/specflow-livingdoc/en/latest/LivingDocGenerator/Generating-Documentation.html)**
+
+@mytag
+Scenario: Date
+	Given Launch Calculator in DateCalculator Mode
+	#And Screen Clear
+	And Click From
+	And Click Previous
+	And Select 2
+	And Click To
+	And ClickPrevious
+	And Select 10
+	Then The result should be 1 week, 1 day
+	And End Calculator
---
+++ CalculatorSteps.cs
@@ -0,0 +1,87 @@
+﻿using FluentAssertions;
+//using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using System;
+using System.Threading;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowDateCalculator.Features
+{
+    [Binding]
+    public class CalculatorSteps
+    {
+
+        private WindowsDriver<WindowsElement> _driver;
+        private object calculatorResult;
+
+        [Given(@"Launch Calculator in DateCalculator Mode")]
+        public void GivenLaunchCalculatorInDateCalculatorMode()
+        {
+            var options = new AppiumOptions();
+            options.AddAdditionalCapability("app", "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
+            options.AddAdditionalCapability("deviceName", "WindowsPC");
+            _driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), options);
+            _driver.FindElementByName("Open Navigation").Click();
+            _driver.FindElementByName("Date Calculation Calculator").Click();
+            //_driver.FindElementByName("Clear").Click();
+
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+
+        //[Given(@"Screen Clear")]
+        //public void GivenScreenClear()
+        //{
+        //    _driver.FindElementByName("Clear").Click();
+        //}
+        
+        [Given(@"Click From")]
+        public void GivenClickFrom()
+        {
+            _driver.FindElementByName("From").Click();
+        }
+        
+        [Given(@"Click Previous")]
+        public void GivenClickPrevious()
+        {
+            
+            _driver.FindElementByName("Previous").Click();
+        }
+        
+        [Given(@"Select (.*)")]
+        public void GivenSelect()
+        {
+            Thread.Sleep(5000);
+            _driver.FindElementByName("4").Click();
+        }
+        
+        [Given(@"Click To")]
+        public void GivenClickTo()
+        {
+            _driver.FindElementByName("To").Click();
+        }
+        
+        [Given(@"ClickPrevious")]
+        public void GivenClickprevious()
+        {
+            _driver.FindElementByName("Previous").Click();
+        }
+
+        [Given(@"Select (.*)")]
+        public void Givenselect(int p0)
+        {
+            _driver.FindElementByName("10").Click();
+        }
+        [Then(@"The result should be (.*) week, (.*) day")]
+        public object ThenTheResultShouldBeWeekDay(int p0, int p1)
+        {
+            return calculatorResult;
+        }
+        
+        [Then(@"End Calculator")]
+        public void ThenEndCalculator()
+        {
+            _driver.FindElementByName("Close Calculator").Click();
+        }
+    }
+}
